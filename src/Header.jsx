1d0c58f863bdc8b4acec1e61cd406158f0dbd5ac
Reginald J Dixon <vassarjonez@yahoo.com>
2024-05-16 12:23:29 -0400

    update

--- src/Header.jsx
+++ src/Header.jsx
@@ -9,34 +9,60 @@ const Navbar = () => {
   };
 
   return (
-    <div>
-      <a
-        href="#menu"
-        id="toggle"
-        className={isMenuOpen ? "on" : ""}
-        onClick={handleToggle}
+    <div className="container">
+      <div
+        className="logo-container"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          margin: "0 auto",
+        }}
       >
-        <span></span>
-      </a>
-
-      <div id="menu" className={isMenuOpen ? "visible" : ""}>
-        <ul>
-          <li>
-            <a href="#home">Home</a>
-          </li>
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
-        </ul>
+        <img
+          src="https://i.ibb.co/P4ccjgN/jrome-stu-logo.png"
+          alt="jrome brand logo"
+          className="logo"
+          style={{ width: "18rem" }}
+        />
       </div>
+      <div>
+        <a
+          href="#menu"
+          id="toggle"
+          className={isMenuOpen ? "on" : ""}
+          onClick={handleToggle}
+        >
+          <span></span>
+        </a>
 
-      <p>
-        Inspired by{" "}
-        <a href="https://codepen.io/loredonut/pen/hjuai">Loredonut Pen</a>
-      </p>
+        <div id="menu" className={isMenuOpen ? "visible" : ""}>
+          <ul>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+
+        <p style={{ width: "50%", margin: "0 auto" }}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo unde
+          quo, quaerat nam quae sit ex a sed ullam aliquid hic alias, debitis
+          atque voluptas. Sunt, incidunt perspiciatis unde, aliquid quod quia
+          dolorum corporis labore distinctio, accusamus debitis. Nemo
+          praesentium soluta quos. Qui corrupti quam nulla cupiditate pariatur,
+          dolor labore harum iure hic ducimus? Repellendus nam maiores porro
+          magni! Fugiat natus dolore, itaque maxime expedita dolorum distinctio
+          non assumenda ex debitis mollitia hic dignissimos facere ab
+          perspiciatis sit odit numquam aliquam ipsum veniam, nobis aut, vitae
+          architecto enim! Magnam, eveniet? Nulla nesciunt, quasi non pariatur
+          nihil adipisci perspiciatis! Molestias, maxime!
+        </p>
+      </div>
     </div>
   );
 };
